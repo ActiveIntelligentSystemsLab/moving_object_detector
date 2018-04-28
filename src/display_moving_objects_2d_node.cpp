b02e793d3bd44e0fedbd15e07e39a90419d9c94b
Hironori Fujimoto <fujimoto@aisl.cs.tut.ac.jp>
2018-04-28 19:08:45 +0900

    Add node for draw bounding boxes of moving object on image

---
+++ src/display_moving_objects_2d_node.cpp
@@ -0,0 +1,65 @@
+#include <cv_bridge/cv_bridge.h>
+#include <image_geometry/pinhole_camera_model.h>
+#include <image_transport/camera_common.h>
+#include <image_transport/subscriber_filter.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
+
+#include <moving_object_detector/MovingObjectArray.h>
+
+image_transport::Publisher image_pub;
+
+void dataCB(const sensor_msgs::ImageConstPtr& image, const sensor_msgs::CameraInfoConstPtr& camera_info, const moving_object_detector::MovingObjectArrayConstPtr& moving_object_array)
+{
+  cv::Mat cv_image;
+  cv_bridge::CvImagePtr input_bridge;
+  try {
+    input_bridge = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::BGR8);
+    cv_image = input_bridge->image;
+  } catch(cv_bridge::Exception& exception) {
+    ROS_ERROR("Failed to Convert image from msg to cv");
+  }
+
+  image_geometry::PinholeCameraModel camera_model;
+  camera_model.fromCameraInfo(camera_info);
+
+  for (auto& moving_object : moving_object_array->moving_object_array) {
+    cv::Point3d top_left_pt(moving_object.center.x - (moving_object.bounding_box.x / 2), moving_object.center.y - (moving_object.bounding_box.y / 2), moving_object.center.z - (moving_object.bounding_box.z / 2));
+
+    cv::Point2d top_left_uv = camera_model.project3dToPixel(top_left_pt);
+    cv::Point3d bottom_right_pt(moving_object.center.x + (moving_object.bounding_box.x / 2), moving_object.center.y + (moving_object.bounding_box.y / 2), moving_object.center.z + (moving_object.bounding_box.z / 2));
+    cv::Point2d bottom_right_uv = camera_model.project3dToPixel(bottom_right_pt);
+
+    cv::rectangle(cv_image, top_left_uv, bottom_right_uv, CV_RGB(255, 0, 0), 2);
+  }
+
+  image_pub.publish(input_bridge->toImageMsg());
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "display_moving_objects_2d");
+  ros::NodeHandle nh;
+
+  image_transport::ImageTransport img_trans = image_transport::ImageTransport(nh);
+  std::string input_image_topic = nh.resolveName("input_image"); // image_transport::SubscriberFilter は何故か名前解決してくれないので
+  std::string input_camerainfo_topic = image_transport::getCameraInfoTopic(input_image_topic);
+
+  image_pub = img_trans.advertise("moving_objects_image", 1);
+  image_transport::SubscriberFilter image_sub;
+  message_filters::Subscriber<sensor_msgs::CameraInfo> camerainfo_sub;
+  message_filters::Subscriber<moving_object_detector::MovingObjectArray> moving_objects_sub;
+
+  image_sub.subscribe(img_trans, input_image_topic, 10);
+  moving_objects_sub.subscribe(nh, "moving_objects", 10);
+  camerainfo_sub.subscribe(nh, input_camerainfo_topic, 10);
+  message_filters::TimeSynchronizer<sensor_msgs::Image, sensor_msgs::CameraInfo, moving_object_detector::MovingObjectArray> sync(image_sub, camerainfo_sub, moving_objects_sub, 10);
+  sync.registerCallback(boost::bind(&dataCB, _1, _2, _3));
+
+  ros::spin();
+
+  return 0;
+}
