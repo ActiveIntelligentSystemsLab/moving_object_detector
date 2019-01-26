829efb3c835c9163b142afc835682bf8dffd5777
Hironori Fujimoto <fujimoto@aisl.cs.tut.ac.jp>
2019-01-27 01:17:21 +0900

    InputSynchronizerクラスを作成 もともとVelocityEstimatorの内部クラスだったが，VeloctiyEstimatorがごちゃごちゃしていたので分離

---
+++ include/input_synchronizer.h
@@ -0,0 +1,49 @@
+#ifndef __HEADER_INPUT_SYNCHRONIZER__
+#define __HEADER_INPUT_SYNCHRONIZER__
+
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <moving_object_detector/InputSynchronizerPublish.h>
+#include <image_transport/subscriber_filter.h>
+#include <image_transport/camera_common.h>
+
+class InputSynchronizer {
+private:
+  ros::NodeHandle node_handle_;
+  
+  ros::ServiceServer publish_service_;
+  
+  std::shared_ptr<image_transport::ImageTransport> image_transport_;
+  
+  image_transport::CameraPublisher left_rect_image_pub_;
+  image_transport::CameraPublisher right_rect_image_pub_;
+  
+  image_transport::SubscriberFilter left_rect_image_sub_;
+  message_filters::Subscriber<sensor_msgs::CameraInfo> left_rect_info_sub_;
+  image_transport::SubscriberFilter right_rect_image_sub_;
+  message_filters::Subscriber<sensor_msgs::CameraInfo> right_rect_info_sub_;
+  
+  typedef message_filters::TimeSynchronizer<sensor_msgs::Image, sensor_msgs::CameraInfo, sensor_msgs::Image, sensor_msgs::CameraInfo> DataTimeSynchronizer;
+  std::shared_ptr<DataTimeSynchronizer> time_sync_;
+  
+  sensor_msgs::CameraInfo left_camera_info_;
+  sensor_msgs::Image left_rect_image_;
+  sensor_msgs::CameraInfo left_rect_info_;
+  sensor_msgs::Image right_rect_image_;
+  sensor_msgs::CameraInfo right_rect_info_;
+  
+  ros::Time last_publish_time_;
+  
+  void publish();
+  
+  void dataCallBack(const sensor_msgs::ImageConstPtr& left_rect_image, const sensor_msgs::CameraInfoConstPtr& left_rect_info, const sensor_msgs::ImageConstPtr& right_rect_image, const sensor_msgs::CameraInfoConstPtr& right_rect_info);
+  bool publishServiceCallback(moving_object_detector::InputSynchronizerPublish::Request &request, moving_object_detector::InputSynchronizerPublish::Response &response);
+  
+public:
+  InputSynchronizer();
+};
+
+#endif
---
+++ src/input_synchronizer.cpp
@@ -0,0 +1,57 @@
+#include "input_synchronizer.h"
+
+InputSynchronizer::InputSynchronizer()
+{
+  image_transport_ = std::make_shared<image_transport::ImageTransport>(node_handle_);
+  
+  std::string publish_left_rect_image_topic = node_handle_.resolveName("synchronizer_output_left_rect_image");
+  std::string publish_right_rect_image_topic = node_handle_.resolveName("synchronizer_output_right_rect_image");
+  
+  left_rect_image_pub_ = image_transport_->advertiseCamera(publish_left_rect_image_topic, 1);
+  right_rect_image_pub_ = image_transport_->advertiseCamera(publish_right_rect_image_topic, 1);
+  
+  std::string subscribe_left_rect_image_topic = node_handle_.resolveName("synchronizer_input_left_rect_image");
+  std::string subscribe_right_rect_image_topic = node_handle_.resolveName("synchronizer_input_right_rect_image");
+  
+  left_rect_image_sub_.subscribe(*image_transport_, subscribe_left_rect_image_topic, 10);
+  left_rect_info_sub_.subscribe(node_handle_, image_transport::getCameraInfoTopic(subscribe_left_rect_image_topic), 10);
+  right_rect_image_sub_.subscribe(*image_transport_, subscribe_right_rect_image_topic, 10);
+  right_rect_info_sub_.subscribe(node_handle_, image_transport::getCameraInfoTopic(subscribe_right_rect_image_topic), 10);
+  time_sync_ = std::make_shared<DataTimeSynchronizer>(left_rect_image_sub_, left_rect_info_sub_, right_rect_image_sub_, right_rect_info_sub_, 10);
+  time_sync_->registerCallback(boost::bind(&InputSynchronizer::dataCallBack, this, _1, _2, _3, _4));
+  
+  publish_service_ = node_handle_.advertiseService("input_synchronizer_publish", &InputSynchronizer::publishServiceCallback, this);
+}
+
+void InputSynchronizer::dataCallBack(const sensor_msgs::ImageConstPtr& left_rect_image, const sensor_msgs::CameraInfoConstPtr& left_rect_info, const sensor_msgs::ImageConstPtr& right_rect_image, const sensor_msgs::CameraInfoConstPtr& right_rect_info)
+{
+  static int count = 0;
+  
+  left_rect_image_ = *left_rect_image;
+  left_rect_info_ = *left_rect_info;
+  right_rect_image_ = *right_rect_image;
+  right_rect_info_ = *right_rect_info;
+  
+  // VelocityEstimator内でinput_synchronizer->publish()を行わない限り処理は開始しないので，初期2フレーム分のデータを送信する
+  if (count < 2) {
+    publish();
+    count++;
+  }
+  else if ((ros::Time::now() - last_publish_time_).toSec() > 0.5) { // publishが途中で停止した場合には復帰
+    publish();
+  }
+}
+
+bool InputSynchronizer::publishServiceCallback(moving_object_detector::InputSynchronizerPublish::Request &request, moving_object_detector::InputSynchronizerPublish::Response &response)
+{
+  publish();
+  return true;
+}
+
+void InputSynchronizer::publish()
+{
+  left_rect_image_pub_.publish(left_rect_image_, left_rect_info_);
+  right_rect_image_pub_.publish(right_rect_image_, right_rect_info_);
+  
+  last_publish_time_ = ros::Time::now();
+}
