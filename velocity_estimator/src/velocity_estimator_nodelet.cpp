78296f61d2df6d3a9dcbb2727a8de19dc174e9bb
Hironori Fujimoto <fujimoto@aisl.cs.tut.ac.jp>
2019-04-09 13:43:14 +0900

    Change namespace of published topic to private

--- velocity_estimator/src/velocity_estimator_nodelet.cpp
+++ velocity_estimator/src/velocity_estimator_nodelet.cpp
@@ -20,14 +20,14 @@ void VelocityEstimatorNodelet::onInit() {
   ros::NodeHandle &node_handle = getNodeHandle();
   ros::NodeHandle &private_node_handle = getPrivateNodeHandle();
 
-  image_transport.reset(new image_transport::ImageTransport(node_handle));
+  image_transport.reset(new image_transport::ImageTransport(private_node_handle));
 
   reconfigure_server_.reset(new ReconfigureServer(private_node_handle));
   reconfigure_func_ = boost::bind(&VelocityEstimatorNodelet::reconfigureCB, this, _1, _2);
   reconfigure_server_->setCallback(reconfigure_func_);
   
-  pc_with_velocity_pub_ = node_handle.advertise<sensor_msgs::PointCloud2>("velocity_pc", 10);
-  static_flow_pub_ = node_handle.advertise<dense_flow_msg::DenseFlow>("static_flow", 1);
+  pc_with_velocity_pub_ = private_node_handle.advertise<sensor_msgs::PointCloud2>("velocity_pc", 10);
+  static_flow_pub_ = private_node_handle.advertise<dense_flow_msg::DenseFlow>("static_flow", 1);
   velocity_image_pub_ = image_transport->advertise("velocity_image", 1);
   
   camera_transform_sub_.subscribe(node_handle, "camera_transform", 1);
